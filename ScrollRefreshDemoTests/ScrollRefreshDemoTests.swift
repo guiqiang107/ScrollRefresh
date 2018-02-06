f037a0420b2c4d4a3fc963e86ab6083d0e616674
guiqiang107 <guiqiang107@gmail.com>
2018-02-06 16:26:48 +0800

    Initial Commit

---
+++ ScrollRefreshDemoTests/ScrollRefreshDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScrollRefreshDemoTests.swift
+//  ScrollRefreshDemoTests
+//
+//  Created by hyku on 2018/2/6.
+//  Copyright © 2018年 hyku. All rights reserved.
+//
+
+import XCTest
+@testable import ScrollRefreshDemo
+
+class ScrollRefreshDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
